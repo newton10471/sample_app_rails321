cac5fef7adc976506dcaa7c7929eb7b16d925355
Matt Newton <newton10471@gmail.com>
2012-02-09 20:18:49 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleAppRails321::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  to: 'users#new'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +59,6 @@ SampleAppRails321::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index
-  root to: 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
